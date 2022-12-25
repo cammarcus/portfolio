7f83a1441f4a41966fe37d9c0a3fae53846d5007
Cameron Marcus <cmarcus9@jhu.edu>
2022-12-25 18:12:35 -0500

    some styling

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function myFunction() {
+function scrollDown() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function bigImg(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+}
+
+function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
 }
