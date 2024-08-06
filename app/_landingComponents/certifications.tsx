84e5712b95415872e9db6cf0d5b172824daa2894
Cameron Marcus <cammarcus19@gmail.com>
2024-08-05 22:08:44 -0400

    certs cleaned

--- app/_landingComponents/certifications.tsx
+++ app/_landingComponents/certifications.tsx
@@ -2,32 +2,29 @@ import Image from "next/image";
 
 export default function Certifications() {
     return (
-        <div className="">
-
-            <div className="flex flex-row">
-                <div className='w-32 h-32 mr-4'>
-                    <div className="relative w-full h-full">
-                        <Image
-                            src="/AWS_ccp.png"
-                            alt="CCP Cert"
-                            quality={100}
-                            fill
-                            priority
-                            style={{ objectFit: "contain" }}
-                        />
-                    </div>
+        <div className="flex flex-row justify-start">
+            <div className='w-32 h-32 mr-4'>
+                <div className="relative w-full h-full">
+                    <Image
+                        src="/AWS_ccp.png"
+                        alt="CCP Cert"
+                        quality={100}
+                        fill
+                        priority
+                        style={{ objectFit: "contain" }}
+                    />
                 </div>
-                <div className='w-32 h-32'>
-                    <div className="relative w-full h-full">
-                        <Image
-                            src="/AWS_dev_assoc.png"
-                            alt="CCP Cert"
-                            quality={100}
-                            fill
-                            priority
-                            style={{ objectFit: "contain" }}
-                        />
-                    </div>
+            </div>
+            <div className='w-32 h-32'>
+                <div className="relative w-full h-full">
+                    <Image
+                        src="/AWS_dev_assoc.png"
+                        alt="CCP Cert"
+                        quality={100}
+                        fill
+                        priority
+                        style={{ objectFit: "contain" }}
+                    />
                 </div>
             </div>
         </div>
