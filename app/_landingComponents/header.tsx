c8ba9379d2f534d0b798285e2ea34f0b77863f13
Cameron Marcus <cammarcus19@gmail.com>
2024-08-05 23:11:09 -0400

    lint fixed

--- app/_landingComponents/header.tsx
+++ app/_landingComponents/header.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 
 export default function Header() {
+
+  const description = 'Full-Stack Developer at Essent Guaranty with expertise in React, Python, and AWS, leveraging a strong foundation in applied mathematics and computer science from Johns Hopkins University. Currently working on "The True Price of a Mocha," a project combining technology with sustainability to drive meaningful change.'
   return (
     <div className="text-gray-800 py-6 px-4 flex flex-col items-center">
       <div className="text-center">
         <div>
-        <p className="text-4xl font-bold mb-2">Cameron Marcus</p>
+          <p className="text-4xl font-bold mb-2">Cameron Marcus</p>
         </div>
         <div className="mt-8">
-        <p className="text-xl mt-2 max-w-4xl mx-auto">
-          Full-Stack Developer at Essent Guaranty with expertise in React, Python, and AWS, leveraging a strong foundation in applied mathematics and computer science from Johns Hopkins University. Currently working on "The True Price of a Mocha," a project combining technology with sustainability to drive meaningful change.
-        </p>
+          <p className="text-xl mt-2 max-w-4xl mx-auto">
+            {description}
+          </p>
         </div>
       </div>
     </div>
